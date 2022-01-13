71b023d75bf6f7001b495f0874e2ae74e304647f
Elozino A. Ovedhe <ovedhee@gmail.com>
2022-01-13 22:31:35 +0000

    fixed bugs

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -5,14 +5,15 @@ import { FaLightbulb, FaRegLightbulb, FaBars, FaTimes } from "react-icons/fa";
 import { IconContext } from "react-icons";
 
 function Header() {
-  const [click, setClick] = useState(false);
+  const [click, setClick] = useState(true);
+  const [themeIcon, setThemeIcon] = useState(true);
   const handleClick = () => setClick(!click);
 
   const theme = () => {
-    click
+    themeIcon
       ? document.body.classList.remove("light")
       : document.body.classList.add("light");
-    setClick(!click);
+      setThemeIcon(!themeIcon);
   };
 
   return (
@@ -22,13 +23,13 @@ function Header() {
           <img src={BrandLogo} alt="brand-logo" />
         </a>
         <div
-          className={click ? "theme" : "theme active"}
+          className={click ? "theme active" : "theme"}
           title="Change theme"
           onClick={theme}
         >
-          {click ? <FaRegLightbulb /> : <FaLightbulb />}
+          {themeIcon ? <FaRegLightbulb /> : <FaLightbulb />}
         </div>
-        <nav className={click ? "header-nav active" : "header-nav"}>
+        <nav className={click ? "header-nav " : "header-nav active"}>
           <ul>
             <li>
               <a href="/">&lt;HOME &#47;&gt;</a>
@@ -45,7 +46,7 @@ function Header() {
           </ul>
         </nav>
         <button className="mobile-nav" onClick={handleClick}>
-          {click ? <FaTimes /> : <FaBars />}
+          {click ? <FaBars /> : <FaTimes />}
         </button>
         {/**Edit the icon to use react-icon */}
       </header>
--- src/component/Project/Data/ProjectData.jsx
+++ src/component/Project/Data/ProjectData.jsx
@@ -1,56 +1,131 @@
-import roomHomepage from '../Assets/room-home-page.png'
-import bookmark from '../Assets/Bookmark-img.png'
-import TodoList from '../Assets/todo-img.png'
-import EasyBank from '../Assets/EasyBank-img.png'
-import RockPaperScissors from '../Assets/Rock-paper-scissors-img.png'
-import Fylo from '../Assets/huddle.png'
+import roomHomepage from "../Assets/room-home-page.png";
+import bookmark from "../Assets/Bookmark-img.png";
+import TodoList from "../Assets/todo-img.png";
+import EasyBank from "../Assets/EasyBank-img.png";
+import RockPaperScissors from "../Assets/Rock-paper-scissors-img.png";
+import Fylo from "../Assets/huddle.png";
 
 export const projectOne = {
-    projectName: 'Room Homepage',
-    projectTool: ["HTML", "CSS", "JAVASCRIPT"],
-    projectDesc: 'In God I trust',
-    projectImage: roomHomepage,
-    projectAlt: 'room-homepage',
-    liveSite: "https://elozino.github.io/room-homepage/"
-}
+  projectName: "Room Homepage",
+  projectTool: ["HTML", "CSS", "JAVASCRIPT"],
+  projectDesc: "In God I trust",
+  projectImage: roomHomepage,
+  projectAlt: "room-homepage",
+  liveSite: "https://elozino.github.io/room-homepage/",
+};
 export const projectTwo = {
-    projectName: 'Bookmark',
-    projectTool: [['HTML', 'CSS', 'JS']],
-    projectDesc: 'In God I trust',
-    projectImage: bookmark,
-    projectAlt: 'bookmaark',
-    liveSite: "https://bookmark-landing-page-ecru.vercel.app/"
-}
+  projectName: "Bookmark",
+  projectTool: [["HTML", "CSS", "JS"]],
+  projectDesc: "In God I trust",
+  projectImage: bookmark,
+  projectAlt: "bookmaark",
+  liveSite: "https://bookmark-landing-page-ecru.vercel.app/",
+};
 export const projectThree = {
-    projectName: 'Todo App',
-    projectTool: ['HTML', 'CSS', 'JS'],
-    projectDesc: 'In God I trust',
-    projectImage: TodoList,
-    projectAlt: 'todolist',
-    liveSite: 'https://todo-list-elozino.vercel.app/'
-}
+  projectName: "Todo App",
+  projectTool: ["HTML", "CSS", "JS"],
+  projectDesc: "In God I trust",
+  projectImage: TodoList,
+  projectAlt: "todolist",
+  liveSite: "https://todo-list-elozino.vercel.app/",
+};
 export const projectFour = {
-    projectName: 'EasyBank',
-    projectTool: ['HTML', 'CSS', 'JS'],
-    projectDesc: 'In God I trust',
-    projectImage: EasyBank,
-    projectAlt: 'easybank',
-    liveSite: "https://easybank-landing-page-elozino.vercel.app/"
-}
+  projectName: "EasyBank",
+  projectTool: ["HTML", "CSS", "JS"],
+  projectDesc: "In God I trust",
+  projectImage: EasyBank,
+  projectAlt: "easybank",
+  liveSite: "https://easybank-landing-page-elozino.vercel.app/",
+};
 export const projectFive = {
-    projectName: 'Rock Paper Scissors',
-    projectTool: ['HTML', 'CSS', 'JS'],
-    projectDesc: 'In God I trust',
-    projectImage: RockPaperScissors,
-    projectAlt: 'rock  paper scissors',
-    liveSite: "https://zrock-paper-scissors.netlify.app/"
-}
+  projectName: "Rock Paper Scissors",
+  projectTool: ["HTML", "CSS", "JS"],
+  projectDesc: "In God I trust",
+  projectImage: RockPaperScissors,
+  projectAlt: "rock  paper scissors",
+  liveSite: "https://zrock-paper-scissors.netlify.app/",
+};
 export const projectSix = {
-    projectName: 'Fylo',
-    projectTool: ['HTML', 'CSS'],
-    projectDesc: 'In God I trust',
-    projectImage: Fylo,
-    projectAlt: 'fylo',
-    liveSite: "https://fylo-dark-theme-landing-page-elozino.vercel.app/",
-    on: "https://grid-testimonial-roan.vercel.app/"
-}
+  projectName: "Fylo",
+  projectTool: ["HTML", "CSS"],
+  projectDesc: "In God I trust",
+  projectImage: Fylo,
+  projectAlt: "fylo",
+  liveSite: "https://fylo-dark-theme-landing-page-elozino.vercel.app/",
+  on: "https://grid-testimonial-roan.vercel.app/",
+};
+
+
+
+
+
+
+
+
+
+
+
+export const project1 = {
+  projectName: "Todo App",
+  projectTool: ["REACT, TYPESCRIPT"],
+  projectDesc: "In God I trust",
+  projectImage: RockPaperScissors,
+  projectAlt: "Todo App",
+  liveSite: "https://zrock-paper-scissors.netlify.app/",
+};
+export const project2 = {
+  projectName: "Morty App",
+  projectTool: ["REACT", "GRAPHQL", "TAILWIND"],
+  projectDesc: "In God I trust",
+  projectImage: RockPaperScissors,
+  projectAlt: "rock  paper scissors",
+  liveSite: "https://zrock-paper-scissors.netlify.app/",
+};
+export const project3 = {
+  projectName: "Admino Dashboard",
+  projectTool: ["REACT"],
+  projectDesc: "In God I trust",
+  projectImage: RockPaperScissors,
+  projectAlt: "rock  paper scissors",
+  liveSite: "https://zrock-paper-scissors.netlify.app/",
+};
+export const project4 = {
+  projectName: "Crypto App",
+  projectTool: ["REACT", "REDUX"],
+  projectDesc: "In God I trust",
+  projectImage: RockPaperScissors,
+  projectAlt: "rock  paper scissors",
+  liveSite: "https://zrock-paper-scissors.netlify.app/",
+};
+export const project5 = {
+  projectName: "Rock Paper Scissors",
+  projectTool: ["HTML", "CSS", "JS"],
+  projectDesc: "In God I trust",
+  projectImage: RockPaperScissors,
+  projectAlt: "rock  paper scissors",
+  liveSite: "https://zrock-paper-scissors.netlify.app/",
+};
+export const project6 = {
+  projectName: "Rock Paper Scissors",
+  projectTool: ["HTML", "CSS", "JS"],
+  projectDesc: "In God I trust",
+  projectImage: RockPaperScissors,
+  projectAlt: "rock  paper scissors",
+  liveSite: "https://zrock-paper-scissors.netlify.app/",
+};
+export const project7 = {
+  projectName: "Rock Paper Scissors",
+  projectTool: ["HTML", "CSS", "JS"],
+  projectDesc: "In God I trust",
+  projectImage: RockPaperScissors,
+  projectAlt: "rock  paper scissors",
+  liveSite: "https://zrock-paper-scissors.netlify.app/",
+};
+export const project8 = {
+  projectName: "Rock Paper Scissors",
+  projectTool: ["HTML", "CSS", "JS"],
+  projectDesc: "In God I trust",
+  projectImage: RockPaperScissors,
+  projectAlt: "rock  paper scissors",
+  liveSite: "https://zrock-paper-scissors.netlify.app/",
+};
